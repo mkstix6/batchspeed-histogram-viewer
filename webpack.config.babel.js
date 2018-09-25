c8479a9ae9bc69c1d781a92577bc36fe2f770071
Mark Stickling <mkstix6@gmail.com>
2018-09-25 01:46:26 +0100

    General setup

---
+++ webpack.config.babel.js
@@ -0,0 +1,25 @@
+import webpack from 'webpack'
+
+module.exports = {
+  context: __dirname,
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+    ]
+  }
+}
